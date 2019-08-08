48df96e3682fd40a63b67a80b5e1aef815f1f52c
Tyler Spaulding <TylerSpaulding95@gmail.com>
2019-08-07 20:17:44 -0400

    Initial setup of express server and testing of get /api/users request

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your API here
+const express = require('express');
+const server = express();
+const db = require('./data/db.js');
+
+//CRUD operations
+
+//Get array of all users
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then((users)=> {
+            res.json(users);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                err: err,
+            })
+        })
+})
+
+server.listen(6000, ()=> {
+    console.log('server is listening on port 6000');
+});
